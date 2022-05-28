fcabadc3bbfa0942b76b57781147e333708bc5c0
oleg456-g <62814783+oleg456-g@users.noreply.github.com>
2022-05-28 19:52:54 +0300

    v1.0
    Рабочий вариант, но есть над чем поработать.

---
+++ main.py
@@ -0,0 +1,49 @@
+import telebot
+import os
+from config import TOKEN  # Токен бота в Telegram
+from random import randint
+
+# Инициализируем бота
+bot = telebot.TeleBot(TOKEN)
+# Создаём словарь, где ключ - название песни, значение - путь к песни
+audios = dict()
+# Получаем песни из папки
+path_to_files = os.getenv('USERPROFILE')+'\\Desktop\\'
+for file in os.listdir(path_to_files):
+    if file.endswith('.mp3'):
+        audios.update({file[:-4].replace('-', ' '): os.path.join(path_to_files, file)})
+
+
+# Создаём inline кнопки
+keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+button1 = telebot.types.KeyboardButton('Начать игру.')
+keyboard.add(button1)
+
+
+# Проверяем ответ пользователя
+def check_answer(message, audio):
+    correct_answer = audio[0]
+    if message.text.lower() == correct_answer.lower():
+        bot.send_message(message.chat.id, 'Правильно, молодец!')
+
+
+# Создаём функцию в ответ на команду start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет, я развлекательный бот. Попробуй угадать музыку.', reply_markup=keyboard)
+
+
+# Создаём функцию игры
+@bot.message_handler(content_types=['text'])
+def play(message):
+    if message.text == 'Начать игру.':
+        # Путь к аудио трекам
+        audios_path = list(audios.items())
+        audio = audios_path[randint(0, len(audios_path))-1]
+        # Отправляем аудио
+        with open(audio[1], 'rb') as audio_path:
+            msg = bot.send_audio(message.chat.id, audio_path)
+            bot.register_next_step_handler(msg, check_answer, audio)
+
+
+bot.infinity_polling()
